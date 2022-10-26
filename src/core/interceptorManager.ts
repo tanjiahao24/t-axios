1636b9d626689be27fc9d3655cb8fe7c03f0de62
Errors-King <2476987169@qq.com>
2022-10-26 22:55:44 +0800

    feat(interceptorManager.ts): add interceptor

---
+++ src/core/interceptorManager.ts
@@ -0,0 +1,30 @@
+import { Interceptor, RejectedFn, ResolvedFn } from '../types'
+
+export default class InterceptorManager<T> {
+  private interceptors: Array<Interceptor<T> | null>
+  constructor() {
+    this.interceptors = []
+  }
+
+  use(resolved: ResolvedFn<T>, rejected?: RejectedFn): number {
+    let length = this.interceptors.push({
+      resolved,
+      rejected
+    })
+    return length - 1
+  }
+
+  forEach(fn: (interceptor: Interceptor<T>) => void): void {
+    this.interceptors.forEach(interceptor => {
+      if (interceptor !== null) {
+        fn(interceptor)
+      }
+    })
+  }
+
+  eject(id: number): void {
+    if (this.interceptors[id]) {
+      this.interceptors[id] = null
+    }
+  }
+}
